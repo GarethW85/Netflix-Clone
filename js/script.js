e5de82ae9f521cdcabbbb9767bf7bcb4f72014e2
GarethW85 <tidylad@hotmail.com>
2022-09-03 13:54:08 +0200

    Login page created with css styling and  added vuejs code
    Created the login page that redirects to the index page upon successful login.
    
    Added styling to the login page using CSS and animation as well as images on the login page and register page.
    
    form login has some validation but I will sort this out later once I've got most of everything else done.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let app = new Vue({
+  el: "#app",
+
+  data: {
+    registerActive: false,
+    emailLogin: "",
+    passwordLogin: "",
+    emailReg: "",
+    passwordReg: "",
+    confirmReg: "",
+    emptyFields: false },
+
+
+  methods: {
+    doLogin() {
+      if (this.emailLogin === "" || this.passwordLogin === "") {
+        this.emptyFields = true;
+      } else {
+        window.location.href = "/index.html"
+        alert("You are now logged in");
+        
+      }
+    },
+
+    doRegister() {
+      if (this.emailReg === "" || this.passwordReg === "" || this.confirmReg === "") {
+        this.emptyFields = true;
+      } else {
+        alert("Welcome, you are now Registered");
+      }
+    } } });
