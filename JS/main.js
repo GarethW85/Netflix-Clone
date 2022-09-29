ee4b9031743e545c40da098523ae604374d1af90
GarethW85 <tidylad@hotmail.com>
2022-09-29 22:45:42 +0200

    updated the main js file as there was an issue with the previous one
    The main js will was sitting in the wrong folder on github so i moved it to the correct folder and pushes the changes

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,32 @@
 // Main Content Section JS
 
-const main = document.querySelector(".main");
-  fetch(
-  genres_list_http +
-    new URLSearchParams({
-      api_key: api_key,
+const main = document.querySelector('.main');
+
+fetch(genres_list_http + new URLSearchParams({
+    api_key: api_key
+}))
+.then(res => res.json())
+.then(data => {
+    data.genres.forEach(item => {
+        fetchMoviesListByGenres(item.id, item.name);
     })
-)
-  .then((res) => res.json())
-  .then((data) => {
-    data.genres.forEach((item) => {
-      fetchMoviesListByGenres(item.id, item.name);
-    });
-  });
+});
 
 const fetchMoviesListByGenres = (id, genres) => {
-  fetch(
-    movie_genres_http +
-      new URLSearchParams({
+    fetch(movie_genres_http + new URLSearchParams({
         api_key: api_key,
         with_genres: id,
-        page: Math.floor(Math.random() * 3) + 1,
-      })
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      makeCategoryElement(`${genres}_movies`, data.results);
+        page: Math.floor(Math.random() * 3) + 1
+    }))
+    .then(res => res.json())
+    .then(data => {
+        makeCategoryElement(`${genres}_movies`, data.results);        
     })
-    .catch((err) => console.log(err));
-};
+    .catch(err => console.log(err));
+}
 
 const makeCategoryElement = (category, data) => {
-  main.innerHTML += `
+    main.innerHTML += `
     <div class="movie-list">
 
         <button class="pre-btn"><i class="bi bi-chevron-left"></i></button>
@@ -43,30 +38,32 @@ const makeCategoryElement = (category, data) => {
         <button class="nxt-btn"><i class="bi bi-chevron-right"></i></button>
     </div>
     `;
-  makeCards(category, data);
-};
+    makeCards(category,data);
+}
 
 const makeCards = (id, data) => {
-  const movieContainer = document.getElementById(id);
-  data.forEach((item, i) => {
-    if (item.backdrop_path == null) {
-      item.backdrop_path = item.poster_path;
-      if (item.backdrop_path == null) {
-        return;
-      }
-    }
+    const movieContainer = document.getElementById(id);
+    data.forEach ((item, i) =>{
+        if(item.backdrop_path == null){
+            item.backdrop_path = item.poster_path;
+            if(item.backdrop_path == null){
+                return;
+            }
+        }
 
-    movieContainer.innerHTML += `
+        movieContainer.innerHTML += `
         <div class="movie hvr-grow" onclick="location.href = '/search.html'">
             <img src="${img_url}${item.backdrop_path}" alt="" width="300px">
             <p class="movie-title">${item.title}</p>
         </div>
         `;
 
-    if (i == data.length - 1) {
-      setTimeout(() => {
-        setupScrolling();
-      }, 100);
-    }
-  });
-};
+        if(i == data.length - 1){
+            setTimeout(() =>{
+                setupScrolling();
+            }, 100);
+        }
+    })
+}
+
+
